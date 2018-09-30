7b280b0de40c4afb9eced42b848756f14597142a
nicolasprr <niparrara@unal.edu.co>
2018-09-30 12:06:14 -0500

    add column name and lastname to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_30_152842) do
+ActiveRecord::Schema.define(version: 2018_09_30_165941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -136,6 +136,8 @@ ActiveRecord::Schema.define(version: 2018_09_30_152842) do
     t.bigint "userable_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "lastname"
     t.index ["userable_type", "userable_id"], name: "index_users_on_userable_type_and_userable_id"
   end
 
