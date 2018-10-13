2bde1d2ae6063a384fa8e07c7ea9ce814b4c6be1
nicolasprr <niparrara@unal.edu.co>
2018-10-13 13:41:33 -0500

    Add cors to application.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,31 @@
 class UsersController < ApplicationController
   before_action :authenticate_user, only: [:show]
   before_action :set_user, only: [:show]
+
   def current
     render json: current_user
   end
-  
 
   def index
     userm = current_user
-  
+
     userS = []
-    if userm   
-      if userm.userable_type =="Admin"
+    if userm
+      if userm.userable_type == "Admin"
         userCareer = User.getUsersByCareer(userm.career_id)
         render json: {status: "SUCCESS", message: "Loaded users", data: userCareer}, status: :ok
-      elsif userm.userable_type =="Tutor"
-         studTutor = Student.studentsOfTutor(userm.userable_id)#Student.where( tutor_id: userm.userable_id)
-         studTutor.each do |st|
-            userS.push( User.where("userable_type = ? and userable_id = ?","Student" , st.id ).first)
-         end
+      elsif userm.userable_type == "Tutor"
+        studTutor = Student.studentsOfTutor(userm.userable_id) #Student.where( tutor_id: userm.userable_id)
+        studTutor.each do |st|
+          userS.push(User.where("userable_type = ? and userable_id = ?", "Student", st.id).first)
+        end
         render json: {status: "SUCCESS", message: "Loaded students of tutors", data: userS}, status: :ok
-
+      elsif userm.userable_type == "Student"
+        render json: {status: "SUCCESS", message: "Loaded users", data: current_user}, status: :ok
       end
     else
       render json: {status: "SUCCESS", message: "Loaded users", data: current_user}, status: :ok
-
     end
-    
   end
 
   def show
@@ -70,7 +69,6 @@ class UsersController < ApplicationController
 
   # Only allow a trusted parameter "white list" through.
   def user_params
-    params.require(:user).permit(:name, :lastname,:email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :lastname, :email, :password, :password_confirmation)
   end
-  
 end
